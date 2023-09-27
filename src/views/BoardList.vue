f6f61a7368389a9614c3113d6be718ab24730b45
imgonnabe <qorlwn456123@gmail.com>
2023-09-27 12:38:59 +0900

    paging

--- src/views/BoardList.vue
+++ src/views/BoardList.vue
@@ -23,6 +23,14 @@
             </tr>
         </tbody>
     </table>
+    <div class="paging">
+        {{pageNo}}
+      <button v-on:click="board(startpage)">&lt;&lt;</button>
+      <button v-on:click="prevPage">&lt;</button>
+      <button v-for="pageNumber in lastRecordIndex - firstRecordIndex + 1" :key="pageNumber" v-on:click="board(firstRecordIndex + pageNumber - 1)">{{firstRecordIndex + pageNumber - 1}}</button>
+      <button v-on:click="nextPage">&gt;</button>
+      <button v-on:click="board(endpage)">&gt;&gt;</button>
+    </div>
     <button @click="write">글쓰기</button>
   </div>
 </template>
@@ -36,20 +44,55 @@ export default {
             // this.$.route=라우터 상태를 관리하는 객체(현재 탐색 중인 현재 경로를 나타내는 객체)
             list:[],
             requestBody: this.$route.query,// 현재 경로 URL의 쿼리 매개변수
+            pageNo: this.$route.query.pageNo,
+            totalcount: null,
+            totalpage: null,
+            totalpageCount: null,
+            startpage: null,
+            endpage: null,
+            firstRecordIndex: null,
+            lastRecordIndex: null
         };
     },
-    mounted(){
-        this.$axios.get(this.$server + '/board')
-        .then((res) => {//function(res){}
-            // alert(res.data.arr[0].btitle);
-            this.list = res.data.arr;
-        })
-        .catch((err) => {
-            alert('문제가 발생했습니다.boardList ' + err);
-        });
-        
+    mounted(){       
+        this.board(this.requestBody.pageNo);
     },
     methods:{
+        paging(totalcount){
+            this.totalcount = totalcount
+            this.totalpage = Math.ceil(this.totalcount / 10)
+            this.startpage =  1
+            this.endpage = this.totalpage
+            this.firstRecordIndex = this.pageNo % 10 != 0 ? parseInt(this.pageNo / 10) * 10 + 1 : this.pageNo - 9
+            this.lastRecordIndex = this.firstRecordIndex + 9
+            if(this.lastRecordIndex >= this.endpage){
+                this.lastRecordIndex = this.endpage
+            }
+        },
+        prevPage(){
+            if (this.firstRecordIndex > 1) {
+                this.board(this.firstRecordIndex - 1);
+            }
+        },
+        nextPage(){
+            if (this.lastRecordIndex < this.totalpage) {
+                this.board(this.lastRecordIndex + 1);
+            }
+        },
+        board(pageNo){
+            this.requestBody.pageNo = pageNo
+            this.$axios
+            .get(this.$server + '/board', {params:this.requestBody})
+            .then((res) => {//function(res){}
+                // alert(res.data.arr[0].btitle);
+                this.list = res.data.arr;
+                this.pageNo = res.data.pageNo
+                this.paging(res.data.totalcount)          
+            })
+            .catch((err) => {
+                alert('문제가 발생했습니다.boardList ' + err);
+            });
+        },
         viewDetail(bno){
             // alert(bno + '번을 눌렀습니다.')
             this.requestBody.bno = bno
@@ -60,7 +103,7 @@ export default {
         },
         write(){
             this.$router.push("write");
-        }
+        }     
     }
 }
 </script>
