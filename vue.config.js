2e341848c528fc57b77ebb36f1a8c9949dc232ce
imgonnabe <qorlwn456123@gmail.com>
2023-10-04 15:13:49 +0900

    vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: 'C:/Users/user/eclipse-workspace/sep19/src/main/resources/static',
+  devServer: {
+    proxy: 'http://localhost:80'
+  }
 })
